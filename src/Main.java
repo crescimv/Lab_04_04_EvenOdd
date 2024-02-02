2eb1e3382a079a0cf7bced47d5006bf0209e01dc
crescimv <crescimv@mail.uc.edu>
2024-02-02 10:52:23 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        //declarations
+
+        int testNum = 2;
+
+        if (testNum % 2 == 0)
+        {
+            System.out.println("The number is even");
+        }
+            else {
+            System.out.println("The number is odd");
+        }
+    }
+}
